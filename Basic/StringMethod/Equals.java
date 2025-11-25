1f3a8c92ea306c6a3c3e640957685cff95f828f8
chetan07 <cnlavange03@gmail.com>
2025-11-25 23:58:51 +0530

    Equals are used to comaparing two string

---
+++ Basic/StringMethod/Equals.java
@@ -0,0 +1,15 @@
+public class Equals {
+    public static void main(String []args)
+    {
+        String username ="Hyd";
+        if(username.equals("Hyd"))
+        {
+            System.out.println("Welcome to this Application");
+        }
+        else 
+        {
+            System.out.println("Wrong Username");
+        }
+
+    }
+}
