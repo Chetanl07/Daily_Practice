783f43ea28baebc2953b4bd24f7cc57f562ef888
chetan07 <cnlavange03@gmail.com>
2025-11-25 23:48:12 +0530

    read a character of x index value by using charAt method

---
+++ Basic/StringMethod/CharAt.java
@@ -0,0 +1,9 @@
+public class CharAt {
+    public static void main(String []args)
+    {
+        String str="HYDERABAD";
+        char ch=str.charAt(5);
+        System.out.println("Character at 5th position: "+ch);
+    }
+    
+}
