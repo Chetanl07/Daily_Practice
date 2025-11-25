4d249acb339ae86d6891ff4bd06fedb0ec020529
chetan07 <cnlavange03@gmail.com>
2025-11-25 23:54:44 +0530

    concatenate two strings with concate and + string method

---
+++ Basic/StringMethod/Concate.java
@@ -0,0 +1,20 @@
+public class Concate {
+    public static void main(String []args)
+    {
+        String s1="Data";
+        String s2="base";
+
+        String s3=s1.concat(s2);
+        System.out.println(s3);  // Database
+
+        String s4="James ";
+        String s5="Gosling";
+        String s6=s4+s5;
+        System.out.println(s6); // James Gosling
+
+        String s7="Naresh";
+        System.out.println(s7.concat(" technology")); // Naresh technology
+
+
+    }
+}
