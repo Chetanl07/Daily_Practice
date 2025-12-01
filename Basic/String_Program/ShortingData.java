2ec39c378809d68e8cb7b76bd0c9ef479c5e09c3
chetan07 <cnlavange03@gmail.com>
2025-12-01 23:05:19 +0530

    sorting the string

---
+++ Basic/String_Program/ShortingData.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class ShortingData {
+    public static void main(String [] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+
+        char[] ch=str.toCharArray();
+
+        for(int i=0;i<ch.length;i++)
+        {
+            for(int j=i+1;j<ch.length;j++)
+            {
+                if(ch[i]>ch[j])
+                {
+                    char temp=ch[i];
+                    ch[i]=ch[j];
+                    ch[j]=temp;
+                }
+            }
+        }
+
+        System.out.println(new String (ch));
+    }
+    
+}
