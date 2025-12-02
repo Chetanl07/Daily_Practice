f4ba6f2247dc8f6b4a266b294a3f425fd5311b82
chetan07 <cnlavange03@gmail.com>
2025-12-03 00:08:52 +0530

    check String are palindrome or not

---
+++ Basic/String_Program/PalindromeInString.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class PalindromeInString {
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+
+        String str=sc.nextLine();
+
+        boolean isPalindrome=true;
+        for(int i=0;i<str.length()/2;i++)
+        {
+            if(str.charAt(i) != str.charAt(str.length()-1-i))
+            {
+                isPalindrome=false;
+                break;
+            }
+        } 
+        if(isPalindrome) System.out.println(str+" is a palindrome.");
+        else System.out.println(str+" is not a palindrome.");
+    }
+    
+}
