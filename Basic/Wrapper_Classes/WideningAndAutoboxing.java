9ed3bdc342fc24f0075836e3e564385534e9b713
chetan07 <cnlavange03@gmail.com>
2025-12-07 21:42:06 +0530

    widening in javva

---
+++ Basic/Wrapper_Classes/WideningAndAutoboxing.java
@@ -0,0 +1,19 @@
+public class WideningAndAutoboxing {
+    public static void main(String[] args) {
+        long a=12;       //  widening 
+        System.out.println(a);
+
+        Integer b= 15;   // autoboxing
+        System.out.println(b);
+
+        Long c=20L;
+        System.out.println(c);
+
+        Float d=12f;
+        System.out.println(d);
+
+        Double e= 90D;
+        System.out.println(e);
+    }
+    
+}
