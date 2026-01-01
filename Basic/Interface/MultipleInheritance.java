078530dc49b339ed8228a61044d216621e89031d
chetan07 <cnlavange03@gmail.com>
2026-01-01 20:06:33 +0530

    Using interface we can use a multiple inheritance

---
+++ Basic/Interface/MultipleInheritance.java
@@ -0,0 +1,22 @@
+public class MultipleInheritance {
+    public static void main(String[] args) {
+        new Implementer().m1();
+    }
+    
+}
+interface Alpha
+{
+    void m1();
+}
+interface  Beta
+{
+    void m1();
+}
+class Implementer implements  Alpha,Beta
+{
+    @Override
+    public void m1()
+    {
+        System.out.println("Multiple inheritance is possible");
+    }
+}
