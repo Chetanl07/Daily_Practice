25839a817bf341508b74fd9a213055b9fb6997fb
chetan07 <cnlavange03@gmail.com>
2026-01-07 22:31:16 +0530

    Offer for the Prime customer

---
+++ Basic/ScenarioBased/PrimeCustomer.java
@@ -0,0 +1,28 @@
+public class PrimeCustomer extends Customer
+ {
+    protected  double discountRate=10.0;
+
+    public PrimeCustomer(String name)
+    {
+        super(name);
+
+    }
+
+    public double calculateBill(double ...prices)
+    {
+        total=super.calculateBill(prices);
+        return total;
+    }
+
+    public void printDetails()
+    {
+        double discount= (total*discountRate)/100;
+        double finalAmount =total-discount;
+
+        System.out.println("Customer Name is :"+this.name);
+        System.out.println("Total price is : "+this.total);
+        System.out.println("Discount amount is : "+discount);
+        System.out.println("After discount final amount is :"+finalAmount);
+    }
+    
+}
