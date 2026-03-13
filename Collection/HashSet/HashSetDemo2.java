411d27aefe56ac0dddaf618d1c06cebe5f98a1c9
chetan07 <cnlavange03@gmail.com>
2026-03-13 22:55:16 +0530

    check duplicate based on the hash code

---
+++ Collection/HashSet/HashSetDemo2.java
@@ -0,0 +1,18 @@
+// check duplicate  based on the Hash code () 
+import java.util.HashSet;
+
+public class HashSetDemo2 {
+    public static void main(String[] args) {
+        HashSet<String> hs= new HashSet<>();
+        hs.add("Ravi");
+        hs.add("Vijay");
+        hs.add(new String("Ravi"));
+        hs.add("Ajay");
+        hs.add("Palavi");
+        hs.add("Sweta");
+        hs.add(null);
+        hs.add(null);
+        hs.forEach(System.out::println);
+    }
+    
+}
