291b98072a9d4036d61538ca255c0aacff639e09
chetan07 <cnlavange03@gmail.com>
2026-06-17 10:13:14 +0530

    print a data using forEach method

---
+++ Collection/HashMap/HashMapDemo3.java
@@ -0,0 +1,25 @@
+
+import java.util.HashMap;
+
+public class HashMapDemo3 {
+    public static void main(String[] args) {
+        
+        Employee e1= new Employee(101, "Aryan");
+        Employee e2= new Employee(102, "Chetan");
+        Employee e3= new Employee(101, "Aryan");
+        Employee e4= e2;
+
+        HashMap<Employee,String> hm = new HashMap<>();
+        hm.put(e1, "Ameerpet");
+        hm.put(e2, "S.R. Nagar");
+        hm.put(e3, "Begumpet");
+        hm.put(e4, "Panjagutta");
+
+        hm.forEach((k,v)-> System.out.println(k+":"+v));
+    }
+    
+}
+record Employee(Integer empId,String empName)
+{
+
+}
